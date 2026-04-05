6f168890d725f29759e4ed3ae7eb0750732a2fa9
Dennis1227 <dennissilvageroso@gmail.com>
2026-04-05 03:10:50 -0300

    Tarefa Componentes – 2º Bim

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Home } from './components/home/home';
+import { Header } from './template/header/header';
+import { Footer } from './template/footer/footer';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Home, Header, Footer],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
